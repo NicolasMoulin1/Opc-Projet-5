63b4c3d57c1544eb0ab78f32d7de5aafc08d8451
Nicolas <nicolas.moulin104@gmail.com>
2023-11-08 15:26:09 +0100

    ajout de l'effet infini sur la droite et la gauche et ajout des dots qui suive l'image

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,51 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+let index = 0 
+let lastImage = slides.length -1
+let firstImage = 0
+
+const elementImg = document.querySelector(".banner-img")
+const title = document.querySelector("#title")
+
+for (var i = 0; i < slides.length; i++) {
+	const dotElement = document.createElement("span")
+	dotElement.className = "dot"
+    document.querySelector(".dots").appendChild(dotElement)
+	if (i === index) {
+		dotElement.classList.add("dot_selected")
+	}
+}
+
+let dots = document.querySelectorAll(".dots .dot") 
+
+function renderImage(index) {
+	elementImg.setAttribute("src", 'assets/images/slideshow/' + slides[index].image)
+	title.innerHTML = slides[index].tagLine
+}
+
+renderImage(0)
+
+document.querySelector("#arrow_left").addEventListener("click", () => {
+	dots[index].classList.remove("dot_selected")
+	index --
+	if (index < firstImage) {
+		index = lastImage
+	}
+	renderImage(index)
+	dots[index].classList.add("dot_selected")
+	console.log("Click on left")
+})
+
+document.querySelector("#arrow_right").addEventListener("click", () => {
+	dots[index].classList.remove("dot_selected")
+	index ++
+	if (index > lastImage) {
+		index = firstImage
+	}
+	renderImage(index)
+	dots[index].classList.add("dot_selected")
+	console.log("click on right")
+})
+
